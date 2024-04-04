e12462dc25da1da243093d01cb96f006fbed7d0b
fuga-w <f-watanabe@kktsc.onmicrosoft.com>
2024-04-04 16:19:13 +0900

    recognizeTextの本実装

--- Packages/module-tessaract/src/index.ts
+++ Packages/module-tessaract/src/index.ts
@@ -1,5 +1,9 @@
-import { recognize, ImageLike } from "tesseract.js";
+import { createWorker, ImageLike } from "tesseract.js";
 
-export async function recognizeText(image: ImageLike) {
-  return await recognize(image);
+export async function recognizeText(imageSource: ImageLike) {
+  const worker = await createWorker("eng");
+  const ret = await worker.recognize(imageSource);
+  await worker.terminate();
+
+  return ret.data.text;
 }
